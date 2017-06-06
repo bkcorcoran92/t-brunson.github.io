f05996392f1f60fdb501871d9d858332ebc4addb
t-brunson <tbrunson@udel.edu>
2017-06-06 15:41:11 -0400

    Simple Test

--- TestDataConnection.js
+++ TestDataConnection.js
@@ -1,26 +1,10 @@
-(function() {
-myConnector.getData = function(table, doneCallback) {
-        $.getJSON("http://localhost:8888/", function(resp) {
-            var feat = resp.features,
-                tableData = [];
 
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                tableData.push({
-                    "id": feat[i].FormattedID,
-                });
-            }
+ <script type="text/javascript" src="lib/jquery-1.9.1.js"> </script>
 
-            table.appendRows(tableData);
-            doneCallback();
-            console.log(tableData);
-        });
-    };
-      $(document).ready(function() {
-        $("#submitButton").click(function() {
-        console.log(tableData);
-        }
-                                 );
-      }
-                        );
-})();
+   <script>
+    $(document).ready(
+        $.getJSON("http://localhost:8888", function(data){
+            document.write(data);
+
+    
+    </script>  
