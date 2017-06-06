2381bf6eb511480694fec4ccfa99af1613372677
t-brunson <tbrunson@udel.edu>
2017-06-06 11:42:06 -0400

    New Rally WDC

---
+++ testRallyWDC.js
@@ -0,0 +1,48 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            FormattedID: "Formatted ID",
+            dataType: tableau.dataTypeEnum.string
+        }, ];
+
+        var tableSchema = {
+            id: "Project Info",
+            alias: "Trial for pulling data from rally database",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("localhost.8888", function(resp) {
+            var feat = resp.features,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "mag": feat[i].properties.FormattedID,
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "Rally Data"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
