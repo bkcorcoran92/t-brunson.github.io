716a8789087b0a630decadd7fc3e59d59662cfcb
t-brunson <tbrunson@udel.edu>
2017-06-08 11:02:35 -0400

    All Data

--- testRallyWDC.js
+++ testRallyWDC.js
@@ -4,7 +4,15 @@
     myConnector.getSchema = function (schemaCallback) {
     var cols = [
          { id : "FormattedID", alias : "Formatted ID", dataType : tableau.dataTypeEnum.string },
-        
+         { id : "Name", alias : "Name", dataType : tableau.dataTypeEnum.string },
+         { id : "_objectVersion", alias : "Object Verzion", dataType : tableau.dataTypeEnum.float },
+         { id : "_refObjectUUID", alias : "Object UUID", dataType : tableau.dataTypeEnum.string },
+         { id : "_rallyAPIMajor", alias : "API Major", dataType : tableau.dataTypeEnum.float },
+         { id : "_rallyAPIMinor", alias : "API Minor", dataType : tableau.dataTypeEnum.float },
+         { id : "ScheduleState", alias : "Schedule State", dataType : tableau.dataTypeEnum.string },
+         { id : "Blocked", alias : "Blocked", dataType : tableau.dataTypeEnum.boolean },
+         { id : "DirectChildrenCount", alias : "Children Count ", dataType : tableau.dataTypeEnum.float },
+         { id : "_refObjectName", alias : "Object Name", dataType : tableau.dataTypeEnum.string },
     ];
 
     var tableInfo = {
@@ -26,6 +34,15 @@
         for (var i = 0, len = feat.length; i < len; i++) {
             tableData.push({
                 "FormattedID": feat[i].FormattedID,
+                "Name": feat[i]._refObjectName,
+                "_objectVersion": feat[i]._objectVersion,
+                "_refObjectUUID": feat[i]._refObjectUUID,
+                "_rallyAPIMajor": feat[i]._rallyAPIMajor,
+                "_rallyAPIMinor": feat[i]._rallyAPIMinor,
+                "ScheduleState": feat[i].ScheduleState,
+                "Blocked": feat[i].Blocked,
+                "DirectChildrenCount": feat[i].DirectChildrenCount,
+                "_refObjectName": feat[i]._refObjectName,
             });
         }
 
