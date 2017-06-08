b3b640ae2cad85ad1d2f33bb27f70daad57aac10
t-brunson <tbrunson@udel.edu>
2017-06-08 09:43:43 -0400

    redo of class

--- testRallyWDC.js
+++ testRallyWDC.js
@@ -1,47 +1,44 @@
-var feat;
 (function () {
-    
     var myConnector = tableau.makeConnector();
-    
+
     myConnector.getSchema = function (schemaCallback) {
     var cols = [
-        { id : "FormattedID", alias : "Formatted ID", dataType : tableau.dataTypeEnum.string },
-       
+         { id : "FormattedID", alias : "Formatted ID", dataType : tableau.dataTypeEnum.string },
+        
     ];
 
     var tableInfo = {
         id : "RallyData",
-        alias : "Rally Data based on individual workspace/project",
+        alias : "Import of Rally Data",
         columns : cols
     };
 
     schemaCallback([tableInfo]);
 };
-   
+
     myConnector.getData = function(table, doneCallback) {
+        
     $.getJSON("http://localhost:3000", function(resp) {
         var feat = resp,
             tableData = [];
-console.log(feat);
+
         // Iterate over the JSON object
         for (var i = 0, len = feat.length; i < len; i++) {
-             tableData.push({  
-                "FormattedID": feat[i].FormattedID,     
-                 
+            tableData.push({
+                "FormattedID": feat[i].FormattedID,
             });
         }
+
         table.appendRows(tableData);
         doneCallback();
     });
 };
+
     tableau.registerConnector(myConnector);
-        $(document).ready(function () {
-        $("#submitButton").click(function () {
-            tableau.connectionName = "RallyData";
-            tableau.submit();
-            
+    $(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
     });
 });
 })();
-    
-
