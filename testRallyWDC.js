2f24c44e0dd3c74741fd8ddd6a2ca001b270af89
t-brunson <tbrunson@udel.edu>
2017-06-12 11:23:20 -0400

    Multi table

--- testRallyWDC.js
+++ testRallyWDC.js
@@ -2,53 +2,66 @@
     var myConnector = tableau.makeConnector();
 
     myConnector.getSchema = function (schemaCallback) {
-    var cols = [
+        //defining schema to place data
+        
+    var object_cols = [
         { id : "ObjectName", alias : "Object Name", dataType : tableau.dataTypeEnum.string },
+        
         { id : "ObjectUUID", alias : "Object UUID", dataType : tableau.dataTypeEnum.string },
-        { id : "FormattedID", alias : "Formatted ID", dataType : tableau.dataTypeEnum.string },
+        
+        { id : "FormattedID", alias : "Formatted ID", dataType : tableau.dataTypeEnum.string }
+        
+                        ];
+        
+    var objectTable = {
+        id : "Rally Object Data",
+        alias : "Object Data",
+        columns :  object_cols
+    };
+        //iteration data
+        
+    var iteration_cols =[
         { id : "IterationName", alias : "Iteration Name", dataType : tableau.dataTypeEnum.string },
+        
         { id : "IterationUUID", alias : "Iteration UUID", dataType : tableau.dataTypeEnum.string },
-        { id : "ProjectName", alias : "Project Name", dataType : tableau.dataTypeEnum.string },
-        { id : "ProjectUUID", alias : "Project UUID", dataType : tableau.dataTypeEnum.string },
         
+        { id : "ObjectUUID", alias : "Object UUID", dataType : tableau.dataTypeEnum.string }    
         
-    ];
-
-    var tableInfo = {
-        id : "RallyData",
-        alias : "Rally Import Data",
-        columns : cols
+                        ];
+        
+    var iteratoinTable = {
+        id : "Rally Iteration Data",
+        alias : "Iteratoin Data",
+        columns :  iteration_cols
     };
 
-    schemaCallback([tableInfo]);
+    
+
+    schemaCallback([objectTable, iteratoinTable]);
 };
 
     myConnector.getData = function(table, doneCallback) {
+    
     $.getJSON("http://localhost:3000", function(resp) {
         var feat = resp,
             tableData = [];
 
         // Iterate over the JSON object
         for (var i = 0, len = feat.length; i < len; i++) {
-            tableData.push({
-                "ObjectUUID": feat[i]._refObjectUUID,
+            if (feat[i].Iteration == null){
+                     tableData.push({
                 "ObjectName": feat[i]._refObjectName,
                 "FormattedID": feat[i].FormattedID,
-                })
-                if (feat[i].Iteration == null){
-                     tableData.push({
-                    "_refIterationName": feat[i].Iteration
-                     })
-                }
-                else {
-                 tableData.push({
-                "ProjectName": feat[i].Project._refObjectName,
-                "ProjectUUID": feat[i].Project._refObjectUUID,
+                                    })
+                                            }
+            
+             if (feat[i].Iteration !== null){
+            tableData.push({
+                "ObjectUUID": feat[i]._refObjectUUID,
                 "IterationName": feat[i].Iteration._refObjectName,
-                "IterationUUID": feat[i].Iteration._refObjectUUID,
-            })
-                                    }
-        }
+                "IterationUUID": feat[i].Iteration._refObjectUUID,                  })
+                                            }
+                                                        }
             
 
         table.appendRows(tableData);
